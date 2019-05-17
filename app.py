d992646483a135181a760c74412d371dcd72fdba
PJC-1 <jumpzine@gmail.com>
2019-05-17 15:00:56 -0700

    added example route serving some json

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import os
 # Init app
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({ 'msg': 'Hello world'})
+
 # Run Server
 if __name__ == '__main__':
     app.run(debug=True)
